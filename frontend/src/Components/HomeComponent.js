5bd1e705b0f1e0268bcc03db47e5cc5c194d10b7
Binay Dhawa <binaydhawa@outlook.com>
2020-10-16 17:22:17 +1100

    Fixed images css

--- frontend/src/Components/HomeComponent.js
+++ frontend/src/Components/HomeComponent.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import '../Styles/HomeComponent.css'
+import { Button } from 'reactstrap';
+import '../Styles/HomeComponent.css';
+import sc1 from '../Images/skincare/sc1.jpg';
+import sc2 from '../Images/skincare/sc2.jpeg';
+import sc3 from '../Images/skincare/sc3.jpeg';
+import sc4 from '../Images/skincare/sc4.jpeg';
+import sc5 from '../Images/skincare/sc5.jpg';
 
 const HomeComponent = ()=>{
 
@@ -7,10 +13,17 @@ const HomeComponent = ()=>{
         <div class='bodyBackg'>
             <div className='body1container'>
                 <div className='textContainer'>
-                    Should contain some text 
+                    Pick new skin care combination every month upto $200 value. Only 79.90$/month
+                    <button className='shopNowBtn'>Shop Now</button>
                 </div>
                 <div className='ProductContainer'>
-                    Should contain some product images
+                    <div className='ImgContainer'>
+                        <img src={sc2} className='displayImg1'/>
+                        <img src={sc3} className='displayImg1'/>
+                        <img src={sc4} className='displayImg1'/>
+                        <img src={sc5} className='displayImg1'/>
+                        <img src={sc1} className='displayImg1'/>
+                    </div>
                 </div>
 
             </div>
