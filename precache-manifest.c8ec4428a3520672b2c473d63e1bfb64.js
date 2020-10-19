acd8113b759c745be681fd5f8870e163ceeadfad
Binay Dhawa <binaydhawa@outlook.com>
2020-10-19 22:16:08 +1100

    Updates

---
+++ precache-manifest.c8ec4428a3520672b2c473d63e1bfb64.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b38e2cc1a44a2760bc257f9dfe9f69",
+    "url": "/Zeroo/index.html"
+  },
+  {
+    "revision": "fd85de86869fa463164c",
+    "url": "/Zeroo/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "ed7cd178d22248b90f64",
+    "url": "/Zeroo/static/css/main.46ff2a91.chunk.css"
+  },
+  {
+    "revision": "fd85de86869fa463164c",
+    "url": "/Zeroo/static/js/2.85bbf887.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Zeroo/static/js/2.85bbf887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7cd178d22248b90f64",
+    "url": "/Zeroo/static/js/main.2017e500.chunk.js"
+  },
+  {
+    "revision": "3abb6e66698a125de537",
+    "url": "/Zeroo/static/js/runtime-main.00992bfa.js"
+  },
+  {
+    "revision": "d897a034d12363fc750db0fcd60da247",
+    "url": "/Zeroo/static/media/cleansing_cloth.d897a034.jpeg"
+  },
+  {
+    "revision": "dbdc51175778cc8fc44ec5c94ac8a223",
+    "url": "/Zeroo/static/media/daycream_71.dbdc5117.jpeg"
+  },
+  {
+    "revision": "393452f215969394ba5ac01705a998a8",
+    "url": "/Zeroo/static/media/foaming_cleanser.393452f2.jpeg"
+  },
+  {
+    "revision": "808c512a38c2615b74606070cb5370c6",
+    "url": "/Zeroo/static/media/luminous_whip.808c512a.jpeg"
+  },
+  {
+    "revision": "a01042df2c409832f88ff8cc4c97fe49",
+    "url": "/Zeroo/static/media/miracle_boost.a01042df.jpeg"
+  },
+  {
+    "revision": "27b8700fa33573f60823a6ac935c0961",
+    "url": "/Zeroo/static/media/moisturising_lotion.27b8700f.jpeg"
+  },
+  {
+    "revision": "ea5ab54619a38b55a2f1b92f510679bf",
+    "url": "/Zeroo/static/media/night_cream.ea5ab546.jpeg"
+  },
+  {
+    "revision": "2aff9146de4fac1a5e2c624911fabb59",
+    "url": "/Zeroo/static/media/sc1.2aff9146.jpg"
+  },
+  {
+    "revision": "a6dda471a3e1fcc7b04c5d2abf5d2547",
+    "url": "/Zeroo/static/media/sc5.a6dda471.jpg"
+  }
+]);
