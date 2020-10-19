248b256f71669a56b1972d084ac66c46ad91b553
Binay Dhawa <binaydhawa@outlook.com>
2020-10-19 22:15:29 +1100

    changed browser router to hashrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HomeComponent from './Components/HomeComponent';
-import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import {BrowserRouter,Switch,Route, HashRouter} from 'react-router-dom';
 import Nav from './Components/Nav';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -8,13 +8,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
          <Nav/>
         <Switch>
           <Route exact path='/Home' component={HomeComponent}/>
           <Route exact path='/' component={HomeComponent}/>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
